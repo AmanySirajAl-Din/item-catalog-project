e3b31544687695c5de705412fae4beba787a3e72
Amany Siraj <amanysirajaldin@gmail.com>
2017-09-07 14:23:14 +0200

    feat: add food main category template for each main category

--- catalogProject.py
+++ catalogProject.py
@@ -37,7 +37,10 @@ def catalog_latest_updates():
 # or main category clicked
 @app.route('/categories/<int:mainCategory_id>/')
 def mainCategory(mainCategory_id):
-    return "category id = " + mainCategory_id
+    mainCategories = session.query(MainCategory).order_by(asc(MainCategory.name))
+    mainCategory = session.query(MainCategory).filter_by(id=mainCategory_id).one()
+    subCategories = session.query(SubCategory).filter_by(mainCategory_id=mainCategory_id)
+    return render_template('food_main_category.html', mainCategory=mainCategory, mainCategory_id=mainCategory_id, subCategories=subCategories)
 
 
 # sub category
