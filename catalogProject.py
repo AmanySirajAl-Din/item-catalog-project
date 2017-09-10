9628a2965c818b3afac954149eca578c68798a1c
Amany Siraj <amanysirajaldin@gmail.com>
2017-09-10 10:04:59 +0200

    feat: Create anti-forgery state token

--- catalogProject.py
+++ catalogProject.py
@@ -16,6 +16,16 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
+# Create anti-forgery state token
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return "The current session state is %s" % login_session['state']
+
+
 # JSON APIs to view Restaurant Information
 @app.route('/categories/maincategories/JSON/')
 def CategoriesJSON():
