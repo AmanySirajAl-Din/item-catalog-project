3cdf0c40b47f24f7032377079a07649edea3fd8e
Amany Siraj Al-Din <amanysirajaldin@gmail.com>
2017-09-11 14:05:09 +0200

    fix: correct DB name

--- catalogProject.py
+++ catalogProject.py
@@ -23,7 +23,7 @@ CLIENT_ID = json.loads(
 APPLICATION_NAME = "item catalog app"
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///foodCatalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
