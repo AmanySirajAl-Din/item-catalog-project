af70e1cb0afe123d75ae0665cc45a36b5d101c88
Amany Siraj <amanysirajaldin@gmail.com>
2017-09-11 16:00:02 +0200

    fix: delete extra space

--- database_setup_withusers.py
+++ database_setup_withusers.py
@@ -22,7 +22,7 @@ class MainCategory(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     description = Column(String(250))
-     user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
     
     @property
