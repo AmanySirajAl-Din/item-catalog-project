5c30d111e65c4e408efa729e4e8476330374ebe2
Amany Siraj <amanysirajaldin@gmail.com>
2017-09-11 15:55:28 +0200

    fix: add relation between User and MainCategory

--- database_setup_withusers.py
+++ database_setup_withusers.py
@@ -22,6 +22,8 @@ class MainCategory(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     description = Column(String(250))
+     user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
     
     @property
     def serialize(self):
