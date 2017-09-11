55b3f4fd77cccc7b749552cd324f3ff173228f94
Amany Siraj <amanysirajaldin@gmail.com>
2017-09-11 15:54:27 +0200

    refactor: add my details as new user

--- lotsofcategories_withusers.py
+++ lotsofcategories_withusers.py
@@ -12,8 +12,8 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # *Create dummy user*
-User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
-             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+User1 = User(name="Amany Siraj Al-Din", email="amanysirajaldin@gmail.com",
+             picture='https://avatars1.githubusercontent.com/u/24775341?v=4&s=400')
 session.add(User1)
 session.commit()
 
